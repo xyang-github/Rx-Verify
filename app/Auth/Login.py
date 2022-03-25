428a103d38168828620899f01ff0d69a0491b503
alexeischnakenburg <Code4Life@805*>
2022-03-24 19:05:49 -0700

    Updates to login

--- app/Auth/Login.py
+++ app/Auth/Login.py
@@ -1,9 +1,24 @@
-from flask import render_template
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from flask_mail import Mail
+from flask_mail import Message
 from . import main
 
 mail = Mail(main)
 
+class NameForm(FlaskForm):
+    name = StringField('Test1',validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 @main.route("/", methods=["GET", "POST"])
 def index():
+    form =NameForm()
     return render_template("index.html")
+
+#Email support
+def send_email():
+    msg = Message
+    mail.send(msg)
