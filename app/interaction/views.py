028b7fb4539dce45ad5db4f9e6c8d95352f42903
Xing Yang <x.yang89@yahoo.com>
2022-04-27 18:17:37 -0700

    Drug interaction tool will display result

--- app/interaction/views.py
+++ app/interaction/views.py
@@ -4,12 +4,14 @@ from app.interaction.forms import DrugInteractionForm
 from . import interaction
 import requests
 
-list_of_meds = []
+dict_of_meds_name_rxcui = {}
+list_of_med_name = []
 
 
 @interaction.route('/interaction', methods=["GET", "POST"])
 def interaction_main():
     interaction_form = DrugInteractionForm()
+    interaction_list = list()
 
     # When the add button is clicked, will store the drug name and its rxcui as a dictionary in a list
     if interaction_form.btn_add.data:
@@ -18,9 +20,54 @@ def interaction_main():
         if med_name == "":
             flash("Medication name cannot be blank")
         else:
-            list_of_meds.append((med_name, get_rxcui(med_name)))
 
-    return render_template("interaction.html", form=interaction_form, list=list_of_meds)
+            # Create a dictionary of med name and its rxcui
+            dict_of_meds_name_rxcui[med_name] = get_rxcui(med_name)
+
+            # Add med name to the med name list if it's not already there
+            if med_name not in list_of_med_name:
+                list_of_med_name.append(med_name)
+
+    # If the remove button is clicked, will remove the selected med name
+    if interaction_form.btn_remove.data:
+        remove_med = request.form['med_list']
+        list_of_med_name.remove(remove_med)
+
+    # If the submit button is clicked, will show results or an error message
+    if interaction_form.btn_submit.data:
+
+        if len(list_of_med_name) < 2:
+            flash("Must select at least two medications to run an interaction")
+        else:
+
+            # Contains a list of rxcui, which will be used in the web api
+            list_of_rxcui = []
+
+            # Get a list of rxcui from the list of medication names
+            for med in list_of_med_name:
+                list_of_rxcui.append(dict_of_meds_name_rxcui[med])
+
+            drug_string = '+'.join(list_of_rxcui)
+
+            # Construct request url
+            url_base = "https://rxnav.nlm.nih.gov"
+            url_final = url_base + "/REST/interaction/list.json?rxcuis=" + drug_string + "&sources=" + "drugbank"
+
+            # Send a request to API
+            response = requests.get(url_final).json()
+
+            # Getting the drug names, interaction severity and description
+            # Each interactionPair dictionary will contain a list. Each element of the list will contain a severity
+            # and description.
+            for interaction in response['fullInteractionTypeGroup'][0]['fullInteractionType']:
+                interaction_list.append(
+                    [interaction['interactionPair'][0]['interactionConcept'][0]['sourceConceptItem']['name'],
+                     interaction['interactionPair'][0]['interactionConcept'][1]['sourceConceptItem']['name'],
+                     interaction['interactionPair'][0]['severity'],
+                     interaction['interactionPair'][0]['description']]
+                )
+
+    return render_template("interaction.html", form=interaction_form, list=list_of_med_name, result=interaction_list)
 
 
 def get_rxcui(med_name):
